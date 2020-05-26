ad9c3331ea1202f869741b49bd38c998f0c20dc9
paritosh666 <paritosh.ghimire666@gmail.com>
2020-05-26 22:16:29 +0545

    updated debug value

--- sae/productionSettings.py
+++ sae/productionSettings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.stoneageentrepreneur.com', 'stoneageentrepreneur.com']
 
