56517cb338461e895458a237df02720ca783539e
paritosh666 <paritosh.ghimire666@gmail.com>
2020-06-15 14:19:04 +0545

    updated default feature date

--- blog/customcontext.py
+++ blog/customcontext.py
@@ -1,10 +1,13 @@
+from accounts.forms import UserLoginForm, RegistrationForm
 from blog.models import BlogCategory
 
 
 def get_categories(request):
     qs = BlogCategory.objects.active()
     ctx = {
-        'categories': qs
+        'categories': qs,
+        'form': UserLoginForm(),
+        'regForm': RegistrationForm(),
 
     }
     return ctx
