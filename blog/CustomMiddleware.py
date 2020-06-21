80394df329696fdf7fedbb2c6db4ceb91d3f29a7
paritosh666 <paritosh.ghimire666@gmail.com>
2020-06-21 21:09:40 +0545

    fixed page view counter

--- blog/CustomMiddleware.py
+++ blog/CustomMiddleware.py
@@ -11,11 +11,12 @@ class CustomSessionMiddleware:
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         assert hasattr(request, 'user')
+        print(request.path.split('/'))
         if request.path.split('/')[1] == 'posts':
-            if 'has_viewed_post' not in request.session:
+            if 'has_viewed_post_{}'.format(view_kwargs['post_slug']) not in request.session:
                 post = Post.objects.get(slug_name=view_kwargs['post_slug'])
                 post.view_count += 1
                 post.save()
-                request.session['has_viewed_post'] = True
+                request.session['has_viewed_post_{}'.format(view_kwargs['post_slug'])] = True
 
         return None
